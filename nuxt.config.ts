36062147efbff2e62ce7814823291d64239223ca
godkun <godkun666@126.com>
2022-03-02 16:00:26 +0800

    chore 修改title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,16 +3,15 @@ import IconsResolver from "unplugin-icons/resolver";
 import Components from "unplugin-vue-components/vite";
 const lifecycle = process.env.npm_lifecycle_event;
 
-// https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
   meta: {
-    title: "ElementPlus + Nuxt3",
+    title: "m7s",
     meta: [
       { name: "viewport", content: "width=device-width, initial-scale=1" },
       {
         hid: "description",
         name: "description",
-        content: "ElementPlus + Nuxt3",
+        content: "monibuca",
       },
     ],
     link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
@@ -39,5 +38,5 @@ export default defineNuxtConfig({
         resolvers: [IconsResolver({})],
       }),
     ],
-  }
+  },
 });
