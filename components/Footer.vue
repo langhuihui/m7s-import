2f0037eb3e60588b0828edb72c6a3509f40c98ec
dexter <178529795@qq.com>
2022-03-06 21:15:31 +0800

    整一波

--- components/Footer.vue
+++ components/Footer.vue
@@ -47,7 +47,7 @@ const config = ref(data);
     justify-content: center;
     flex: 1;
     img {
-      width: 60%;
+      height: 225px;
     }
   }
   .right {
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -4,10 +4,10 @@ import go from 'highlight.js/lib/languages/go';
 import 'highlight.js/styles/github.css';
 import Roles from '../../components/Roles.vue';
 hljs.registerLanguage('go', go);
-const showCode = ref(false)
+const showCode = ref(false);
 onMounted(() => {
   hljs.highlightAll();
-  showCode.value = true
+  showCode.value = true;
 });
 </script>
 
@@ -16,7 +16,7 @@ onMounted(() => {
     <div class="section">
       <div class="container">
         <!-- <img class="img" src="~@/assets/logo.png" alt /> -->
-        <Roles style="height: 300px;"/>
+        <Roles style="height: 300px;" />
         <div class="intro">
           <div class="title">Monibuca V4</div>
           <div class="info">Go语言开源插件式流媒体服务器开发框架</div>
@@ -29,13 +29,24 @@ onMounted(() => {
       <div class="more">
         <el-row :gutter="16">
           <el-col :span="8">
-            <el-card shadow="hover" class="card"  v-show="showCode">
+            <el-card shadow="never" class="card" v-show="showCode">
               <template #header>
                 <div class="card-header">
                   <span>简洁</span>
                 </div>
-                <div class="card-header go">
-                  <pre>
+                <div class="card-header">
+                  <div class="card-item">代码设计追求极致精简、优雅。</div>
+                </div>
+              </template>
+              <div class="card-header go">
+                <svg xmlns="http://www.w3.org/2000/svg" width="54" height="14" viewBox="0 0 54 14">
+                  <g fill="none" fill-rule="evenodd" transform="translate(1 1)">
+                    <circle cx="6" cy="6" r="6" fill="#FF5F56" stroke="#E0443E" stroke-width=".5" />
+                    <circle cx="26" cy="6" r="6" fill="#FFBD2E" stroke="#DEA123" stroke-width=".5" />
+                    <circle cx="46" cy="6" r="6" fill="#27C93F" stroke="#1AAB29" stroke-width=".5" />
+                  </g>
+                </svg>
+                <pre>
                     <code class="language-go">
 package main
 
@@ -51,50 +62,59 @@ func main() {
 }
                     </code>
                   </pre>
-                </div>
-              </template>
-              <div class="card-item">
-                代码设计追求极致精简、优雅。
               </div>
             </el-card>
           </el-col>
           <el-col :span="8">
-            <el-card shadow="hover">
+            <el-card shadow="never">
               <template #header>
                 <div class="card-header">
                   <span>精致</span>
                 </div>
                 <div class="card-header">
-                  <img class="img" src="~@/assets/monibuca.png" alt />
+                  <div class="card-item">设计精巧的插件机制，实现高内聚低耦合，具有高超的扩展能力</div>
                 </div>
               </template>
-              <div class="card-item">设计精巧的插件机制，实现高内聚低耦合，具有高超的扩展能力</div>
+              <div class="card-header">
+                <img class="img" src="~@/assets/monibuca.png" alt />
+              </div>
             </el-card>
           </el-col>
           <el-col :span="8">
-            <el-card shadow="hover">
+            <el-card shadow="never">
               <template #header>
                 <div class="card-header">
                   <span>易用</span>
                 </div>
+                <div class="card-header">
+                  <div class="card-item">无需配置环境，无需安装运行时，直接下载可执行文件</div>
+                </div>
               </template>
               <div class="card-item">
-                <ul>
-                  <li>没有复杂的环境配置</li>
-                  <li>不需要安装运行时</li>
-                  <li>直接下载可执行文件</li>
-                </ul>
                 <div class="download">
                   <div class="download-item windows">
-                    <i class="iconfont icon-windows"></i>
+                    <a
+                      class="iconfont icon-windows"
+                      href="http://monibuca.com/windows.tgz"
+                      target="_blank"
+                    ></a>
                   </div>
                   <div class="download-item apple">
-                    <i class="iconfont icon-apple"></i>
+                    <a
+                      class="iconfont icon-apple"
+                      href="http://monibuca.com/mac.tgz"
+                      target="_blank"
+                    ></a>
                   </div>
                   <div class="download-item linux">
-                    <i class="iconfont icon-linux"></i>
+                    <a
+                      class="iconfont icon-linux"
+                      href="http://monibuca.com/linux.tgz"
+                      target="_blank"
+                    ></a>
                   </div>
                 </div>
+                <div>windows直接双击exe运行，mac需要设置一下文件的可执行权限</div>
               </div>
             </el-card>
           </el-col>
