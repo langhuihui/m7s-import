4a787c02c00d1d69526cacbe484c2d71fe9e2577
dexter <178529795@qq.com>
2022-05-06 19:08:58 +0800

    被迫修改目录，否则symlink无法消除

--- scripts/copy-readme.js
+++ scripts/copy-readme.js
@@ -3,6 +3,6 @@ const fs = require('fs');
 
 const list = ['rtsp', 'rtmp', 'hdl', 'hls', 'debug', 'record'];
 list.forEach(item => {
-  fs.copyFileSync(`../plugin-${item}/README.md`, `./apps/m7s-website/src/guide/plugin/${item}.md`);
+  fs.copyFileSync(`../plugin-${item}/README.md`, `./apps/m7s-website/src/guide/plugins/${item}.md`);
 });
 
