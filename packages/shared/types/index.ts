0fabc1c55f669b43aec5d4a365e4eecc00a5f914
dexter <178529795@qq.com>
2022-11-02 11:29:36 +0800

    🐛 FIX: 下载链接改为v4

--- packages/shared/types/index.ts
+++ packages/shared/types/index.ts
@@ -1,5 +1,5 @@
 export enum UrlEnum {
-  M7S_WIN = 'https://monibuca.com/windows.tgz',
-  M7S_IOS = 'https://monibuca.com/mac.tgz',
-  M7S_LINUX = 'https://monibuca.com/linux.tgz'
+  M7S_WIN = 'https://m7s.live/bin/m7s_windows_amd64.tar.gz',
+  M7S_IOS = 'https://m7s.live/bin/m7s_darwin_amd64.tar.gz',
+  M7S_LINUX = 'https://m7s.live/bin/m7s_linux_amd64.tar.gz'
 }
